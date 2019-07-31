ca88a2a089d7116d25807d0b8b9059b3bc2717df
evgen7171 <48080044+evgen7171@users.noreply.github.com>
2019-07-31 23:59:34 +0300

    home_work_7
    home_work_7

---
+++ main/config.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'rootName' => $_SERVER['DOCUMENT_ROOT'] .'/../',
+    'name' => 'Мой магазин',
+    'defaultControllerName' => 'good',
+
+    'components' => [
+        'bd' => [
+            'class' => \App\services\BD::class,
+            'config' => [
+                'user' => 'root',
+                'pass' => '',
+                'driver' => 'mysql',
+                'bd' => 'gbphp',
+                'host' => 'localhost:3307',
+                'charset' => 'UTF8',
+            ]
+        ],
+        'userRepository' => [
+            'class' => \App\models\repositories\UserRepository::class
+        ]
+    ],
+];
---
+++ models/entities/Basket.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Админ
+ * Date: 31.07.2019
+ * Time: 23:42
+ */
+
+namespace App\models\entities;
+
+
+class Basket
+{
+    public $userId;
+    public $id;
+    public $price;
+    public $name;
+    public $info;
+}
